e3219a7c532b4182e05763fc9d32a6e8fed24b54
castillejoale <castillejo.alejandro@gmail.com>
2016-05-28 11:10:19 +0200

    Day 1B part 1

---
+++ NoughtsAndCrosses/NoughtsAndCrosses/BoardViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BoardViewController.swift
+//  NoughtsAndCrosses
+//
+//  Created by Alejandro Castillejo on 5/27/16.
+//  Copyright © 2016 Julian Hulme. All rights reserved.
+//
+
+import UIKit
+
+class BoardViewController: UIViewController {
+    
+    @IBOutlet var boardView: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    @IBAction func boardWasTapped(sender: AnyObject) {
+        
+        print("boardWasTapped at index: " + String(sender.tag))
+        
+    }
+    @IBAction func newGameWasTapped(sender: AnyObject) {
+        
+        print("newGameWasTapped")
+        
+    }
+
+}
